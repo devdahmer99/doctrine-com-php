633d48eddad24cc9e5121183b7352297bb3a43d6
Eduardo Dahmer <80227515+devdahmer99@users.noreply.github.com>
2021-09-21 00:27:30 -0300

    Update for my Project

--- cli-config.php
+++ cli-config.php
@@ -1,16 +1,11 @@
 <?php
 
 use Alura\Doctrine\Helper\EntityManagerFactory;
-use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
-try {
-    $entityManager = $entityManagerFactory->getEntityManager();
-} catch (ORMException $e) {
-    echo "Exception is success statement";
-}
+$entityManager = $entityManagerFactory->getEntityManager();
 
 return ConsoleRunner::createHelperSet($entityManager);
